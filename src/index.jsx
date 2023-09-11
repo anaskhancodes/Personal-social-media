8710f7972bf00ee016c118ab03db650201a41d70
M.Anas Khan <a4anas406@gmail.com>
2023-09-11 22:32:33 +0500

    Bio

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Facebook, Instagram, Github, Linkedin, PersonBadgeFill } from 'react-bootstrap-icons';
+import profileImage from './image/Profile.jpg';
+
+function Hi() {
+  return (
+    <div>
+      <div className='pro-card'>
+        <img src={profileImage} alt="" />
+
+        <a href="https://www.facebook.com/Bhoolay.lala/"><div className='social-media'><Facebook className='icons' /> <h1>Facebook</h1> </div></a>
+        <a href="https://www.instagram.com/anaskhan_codes/"> <div className='social-media'><Instagram className='icons' /> <h1>Instagram</h1></div></a>
+        <a href="https://github.com/anaskhancodes"><div className='social-media'><Github className='icons' /> <h1>GitHub</h1></div></a>
+        <a href="https://www.linkedin.com/in/muhammadanaskhancodes/"><div className='social-media'><Linkedin className='icons' /> <h1>Linkedin</h1></div></a>
+        <a href="https://react-resume-cv-rcox.vercel.app/"><div className='social-media'><PersonBadgeFill className='icons' /> <h1>Resume</h1></div></a>
+      </div>
+
+
+    </div>
+  )
+
+}
+
+
+ReactDOM.render(<Hi />, document.querySelector('#root'));
